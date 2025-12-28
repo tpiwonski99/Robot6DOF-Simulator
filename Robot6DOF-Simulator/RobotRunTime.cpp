ab265a7bbb141deee38f5381e765dd495c6beb07
Tymoteusz Piwonski <tymoteuszpiwonski@gmail.com>
2025-12-28 11:54:51 +0100

    Add files via upload

--- Robot6DOF-Simulator/RobotRunTime.cpp
+++ Robot6DOF-Simulator/RobotRunTime.cpp
@@ -35,6 +35,10 @@ RobotRunTime::RobotRunTime(std::shared_ptr<const KinematicModel> model) : model_
 	kinematicsDirty_ = true;
 }
 
+std::size_t RobotRunTime::dof() const {
+	return activeJoints_.size();
+}
+
 void RobotRunTime::setQ(const std::vector<double>& q) {
 
 	if (q.size() != activeJoints_.size())
@@ -57,6 +61,34 @@ void RobotRunTime::setQ(const std::vector<double>& q) {
 		}
 	}
 
+	kinematicsDirty_ = true;
+}
+
+void RobotRunTime::setQd(const std::vector<double>& qdNew) {
+
+	if (qdNew.size() != dof()) 
+		throw std::invalid_argument("[RobotRuntime] setQd(): wrong size.");
+	
+	for (double v : qdNew) {
+		if (!std::isfinite(v)) 
+			throw std::invalid_argument("[RobotRuntime] setQd(): qd contains NaN/Inf.");
+	}
+
+	qd_ = qdNew;
+}
+
+void RobotRunTime::clampToLimits_() {
+	
+	for (std::size_t i = 0; i < activeJoints_.size(); ++i) {
+		const JointId jid = activeJoints_[i];
+		const auto& J = model_->joint(jid);
+
+		if (J.limit.hasLimits) 
+			q_[i] = std::clamp(q_[i], J.limit.lower, J.limit.upper);
+	}
+}
+
+void RobotRunTime::markDirty_() {
 	kinematicsDirty_ = true;
 }
 
@@ -109,4 +141,52 @@ void RobotRunTime::updateKinematicsIfNeeded_() const {
 		T_world_link_[lid] = model_->poseWorld(lid, q_);
 
 	kinematicsDirty_ = false;
+}
+
+const Matrix4& RobotRunTime::linkPoseWorld(LinkId link) const {
+
+	if (link > model_->activeJointCount() || link < 0)
+		throw std::out_of_range("[RobotRuntime] linkPoseWorld(LinkId): link id out of range.");
+
+	updateKinematicsIfNeeded_();
+
+	return T_world_link_[link];
+}
+
+Matrix4 RobotRunTime::linkPoseWorld(const std::string& linkName) const {
+	
+	const LinkId lid = model_->linkId(linkName);
+	return linkPoseWorld(lid);
+}
+
+void RobotRunTime::setJointPosition(JointId id, double value) {
+	
+	if (id >= model_->jointCount()) 
+		throw std::out_of_range("[RobotRuntime] setJointPosition(JointId): joint id out of range.");
+	
+	if (!std::isfinite(value)) 
+		throw std::invalid_argument("[RobotRuntime] setJointPosition(JointId): value is NaN/Inf.");
+	
+
+	const int qi = qIndexOfJoint_.at(id);
+	
+	if (qi < 0) 
+		throw std::invalid_argument("[RobotRuntime] setJointPosition(JointId): joint is fixed (no DOF).");
+	
+
+	q_[static_cast<std::size_t>(qi)] = value;
+
+	const auto& J = model_->joint(id);
+	
+	if (J.limit.hasLimits) {
+		q_[static_cast<std::size_t>(qi)] =
+			std::clamp(q_[static_cast<std::size_t>(qi)], J.limit.lower, J.limit.upper);
+	}
+
+	kinematicsDirty_ = true;
+}
+
+void RobotRunTime::setJointPosition(const std::string& jointName, double value) {
+	const JointId jid = model_->jointId(jointName);
+	setJointPosition(jid, value);
 }
